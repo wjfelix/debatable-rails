06f5c16d4321940af7a8f747aa33a0b1b702184c
Garrick Krol <garrickkrol@hotmail.com>
2015-11-17 18:24:57 -0800

    Fixed integer

--- db/migrate/20151118014044_create_debates.rb
+++ db/migrate/20151118014044_create_debates.rb
@@ -2,7 +2,7 @@ class CreateDebates < ActiveRecord::Migration
   def change
     create_table :debates do |t|
 
-      t.int :owner_id, null: false
+      t.integer :owner_id, null: false
       t.string :topic, null: false
       t.string :type, null: false
       t.string :tok_session_id, null: false
