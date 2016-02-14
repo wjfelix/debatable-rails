ab58249d4fa88ce032f3e2a993c317db90e842cc
rhenvar <rhenvar@github.com>
2016-02-14 17:42:06 +0000

    Updating password via email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,14 @@
 class UserMailer < ActionMailer::Base
+  default :from => 'any_from_address@example.com'
+
   def email_validation(user)
     @user = user
     mail(:to => "#{@user.email}", :subject => "Debatable E-mail Confirmation")
+  end
+
+  def email_password(user, password)
+    @password = password
+    mail(:to => user.email,
+    :subject => 'New Password')
   end
 end
