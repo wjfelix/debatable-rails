e380c2daf2dd067861d0d68e60da4e400b9fe4fd
rhenvar <rhenvar@github.com>
2016-02-16 04:53:33 +0000

    SSL error fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   require 'searchbing'
-
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  
   def index
     if session[:user_id]
       redirect_to home_news_path
