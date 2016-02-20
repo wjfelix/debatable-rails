8e991baec308a9eeed0e5767212fec8e3de90d88
rhenvar <rhenvar@github.com>
2016-02-20 14:14:23 +0000

    5 debaters

--- app/controllers/firetalks_controller.rb
+++ app/controllers/firetalks_controller.rb
@@ -10,7 +10,7 @@ class FiretalksController < ApplicationController
   def new
     @user = User.find(params[:user_id])
     @firetalk = Firetalk.new
-    3.times do
+    4.times do
       @firetalk.firetalk_debaters.build
     end
   end
