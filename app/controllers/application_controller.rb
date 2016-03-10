6ed60ac30475219755c16a83f5baf72ea83457ab
rhenvar <garrickkrol@hotmail.com>
2016-03-09 20:11:48 -0800

    democratic primary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
 
   def grab_new_topic
     # set @topic
-    @topic = "TestTosdfsspisc1"
+    @topic = "Democratic Debate"
 
     # get the topic of the day!!
     response = HTTParty.get('http://www.google.com/trends/hottrends/atom/feed')
